346ea65803e722fcbe89e5f5322e5e34fdc0fa87
Bartosz Laskowski <81393810+Nougat00@users.noreply.github.com>
2021-06-18 23:26:06 +0200

    uno

--- src/pl/edu/pjatk/Main.java
+++ src/pl/edu/pjatk/Main.java
@@ -1,8 +1,17 @@
 package pl.edu.pjatk;
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        JFrame frame = new JFrame("Organizer");
+        frame.setSize(288, 350);
+        JPanel firstScreen = new JPanel(new GridLayout(2,1,5,5));
     }
 }

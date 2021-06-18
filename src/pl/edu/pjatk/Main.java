ffa7b757ac0f14946440a70c2a33f0a9b885d1a0
Bartosz Laskowski <81393810+Nougat00@users.noreply.github.com>
2021-06-19 01:38:19 +0200

    uno

--- src/pl/edu/pjatk/Main.java
+++ src/pl/edu/pjatk/Main.java
@@ -33,6 +33,41 @@ public class Main {
                 if(Login(login.getText(),pass.getText()))
                 {
                     mess.setText("Git");
+                    JFrame Loged = new JFrame("Organizer");
+
+                    JPanel main = new JPanel(new BorderLayout());
+                    Loged.setSize(500, 350);
+                    JPanel text = new JPanel(new GridLayout(8,1,5,5));
+                    JPanel buttons = new JPanel(new GridLayout(1,4,5,5));
+                    JLabel name = new JLabel("Imie i nazwisko");
+                    JTextField namebox= new JTextField();
+                    JLabel date = new JLabel("Data urodzenia");
+                    JTextField datebox= new JTextField();
+                    JLabel adres = new JLabel("Adres zamieszkania");
+                    JTextField adresbox= new JTextField();
+                    JLabel avatar = new JLabel("Avatar");
+                    JTextField avatarbox= new JTextField();
+                    JButton add = new JButton("Dodaj");
+                    JButton modify = new JButton("Zmodyfikuj");
+                    JButton delete = new JButton("Usuń");
+                    JButton search = new JButton("Szukaj");
+                    text.add(name);
+                    text.add(namebox);
+                    text.add(date);
+                    text.add(datebox);
+                    text.add(adres);
+                    text.add(adresbox);
+                    text.add(avatar);
+                    text.add(avatarbox);
+                    buttons.add(add);
+                    buttons.add(modify);
+                    buttons.add(delete);
+                    buttons.add(search);
+                    main.add(text, BorderLayout.NORTH);
+                    main.add(buttons, BorderLayout.CENTER);
+                    Loged.add(main);
+                    Loged.setVisible(true);
+                    frame.setVisible(false);
                 }
                 else
                 {

115711639b2e032a4c04cd30afd1bf9a7dc8513a
Bartosz Laskowski <81393810+Nougat00@users.noreply.github.com>
2021-06-20 21:45:57 +0200

    Work on add panel

--- src/pl/edu/pjatk/Add.java
+++ src/pl/edu/pjatk/Add.java
@@ -2,38 +2,69 @@ package pl.edu.pjatk;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Add {
-//    JFrame Loged = new JFrame("Organizer");
-//    JPanel main = new JPanel(new BorderLayout());
-//    Loged.setSize(500, 350);
-//    JPanel text = new JPanel(new GridLayout(8,1,5,5));
-//    JPanel buttons = new JPanel(new GridLayout(1,4,5,5));
-//    JLabel name = new JLabel("Imie i nazwisko");
-//    JTextField namebox= new JTextField();
-//    JLabel date = new JLabel("Data urodzenia");
-//    JTextField datebox= new JTextField();
-//    JLabel adres = new JLabel("Adres zamieszkania");
-//    JTextField adresbox= new JTextField();
-//    JLabel avatar = new JLabel("Avatar");
-//    JTextField avatarbox= new JTextField();
-//    JButton add = new JButton("Dodaj");
-//    JButton modify = new JButton("Zmodyfikuj");
-//    JButton delete = new JButton("Usuń");
-//    JButton search = new JButton("Szukaj");
-//        text.add(name);
-//        text.add(namebox);
-//        text.add(date);
-//        text.add(datebox);
-//        text.add(adres);
-//        text.add(adresbox);
-//        text.add(avatar);
-//        text.add(avatarbox);
-//        buttons.add(add);
-//        buttons.add(modify);
-//        buttons.add(delete);
-//        buttons.add(search);
-//        main.add(text, BorderLayout.NORTH);
-//        main.add(buttons, BorderLayout.CENTER);
-//        Loged.add(main);
+    public static void window(boolean open) {
+        JFrame frame = new JFrame("Organizer");
+        JPanel main = new JPanel(new BorderLayout());
+        frame.setSize(500, 350);
+        JPanel text = new JPanel(new GridLayout(8, 1, 5, 5));
+        JPanel buttons = new JPanel(new GridLayout(1, 4, 5, 5));
+        JLabel name = new JLabel("Imie i nazwisko");
+        JTextField namebox = new JTextField();
+        JLabel date = new JLabel("Data urodzenia");
+        JTextField datebox = new JTextField();
+        JLabel adres = new JLabel("Adres zamieszkania");
+        JTextField adresbox = new JTextField();
+        JLabel avatar = new JLabel("Avatar");
+        JTextField avatarbox = new JTextField();
+        JLabel message = new JLabel();
+        JButton add = new JButton("Dodaj");
+        JButton back = new JButton("Cofnij");
+        text.add(name);
+        text.add(namebox);
+        text.add(date);
+        text.add(datebox);
+        text.add(adres);
+        text.add(adresbox);
+        text.add(avatar);
+        text.add(avatarbox);
+        buttons.add(add);
+        buttons.add(back);
+        JButton logout = new JButton("Wyloguj");
+        main.add(message, BorderLayout.NORTH);
+        main.add(logout, BorderLayout.SOUTH);
+        main.add(text, BorderLayout.NORTH);
+        main.add(buttons, BorderLayout.CENTER);
+        frame.add(main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        if(open)
+        {
+            frame.setVisible(true);
+        }
+        else
+        {
+            frame.dispose();
+        }
+        logout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                WelcomeScr.window(true);
+            }
+        });
+        add.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!namebox.getText().equals("") && !datebox.getText().equals("") && !adresbox.getText().equals("") && !avatarbox.getText().equals("")) {
+
+                }
+                else {
+                    message.setText("Czegoś brakuje gościu...");
+                }
+            }
+        });
+    }
 }
--- src/pl/edu/pjatk/Choice.java
+++ src/pl/edu/pjatk/Choice.java
@@ -8,55 +8,46 @@ import java.awt.event.ActionListener;
 public class Choice {
 
     public static void window(boolean open) {
-        JFrame Loged = new JFrame("Organizer");
+        JFrame frame = new JFrame("Organizer");
         JPanel main = new JPanel(new BorderLayout());
-        Loged.setSize(500, 350);
-        JPanel text = new JPanel(new GridLayout(8,1,5,5));
+        frame.setSize(500, 180);
         JPanel buttons = new JPanel(new GridLayout(1,4,5,5));
-        JLabel name = new JLabel("Imie i nazwisko");
-        JTextField namebox= new JTextField();
-        JLabel date = new JLabel("Data urodzenia");
-        JTextField datebox= new JTextField();
-        JLabel adres = new JLabel("Adres zamieszkania");
-        JTextField adresbox= new JTextField();
-        JLabel avatar = new JLabel("Avatar");
-        JTextField avatarbox= new JTextField();
         JButton add = new JButton("Dodaj");
         JButton modify = new JButton("Zmodyfikuj");
         JButton delete = new JButton("Usuń");
         JButton search = new JButton("Szukaj");
-        text.add(name);
-        text.add(namebox);
-        text.add(date);
-        text.add(datebox);
-        text.add(adres);
-        text.add(adresbox);
-        text.add(avatar);
-        text.add(avatarbox);
         buttons.add(add);
         buttons.add(modify);
         buttons.add(delete);
         buttons.add(search);
         JButton logout = new JButton("Wyloguj");
         main.add(logout, BorderLayout.SOUTH);
-        main.add(text, BorderLayout.NORTH);
         main.add(buttons, BorderLayout.CENTER);
-        Loged.add(main);
+        frame.add(main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         if(open)
         {
-            Loged.setVisible(true);
+            frame.setVisible(true);
         }
         else
         {
-            Loged.dispose();
+            frame.dispose();
         }
         logout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Loged.dispose();
+                frame.dispose();
                 WelcomeScr.window(true);
             }
         });
+       add.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               Add.window(true);
+               frame.dispose();
+           }
+       });
+
 
     }
 
